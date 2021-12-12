90925733593c34f4a316e8a1c94377486dfd572a
mirahmad <mirahmad.hakimmi@gmail.com>
2021-12-12 14:50:01 +0200

    change

---
+++ constractor.js
@@ -0,0 +1,70 @@
+
+// // constractore is a special method for assigning properties.
+// class Car{
+//     constructor(make,model,year,color,def){
+//         this.make=make;
+//         this.model=model;
+//         this.year=year;
+//         this.color=color;
+//         this.def=def;
+//     }
+// }
+
+//     let car1= new Car("ford","Musting","2033","red");
+//     let car2= new Car("afghan","black","99","blue");
+//     let car3= new Car("tyota","germany","288","pink","neasan");
+//     console.log(car1.make);
+//     console.log(car1.model);
+//     console.log(car1.year);
+//     console.log(car1.color);
+    
+
+//     console.log(car2.make);
+//     console.log(car2.model);
+//     console.log(car2.year);
+//     console.log(car2.color);
+    
+
+//     console.log(car3.make);
+//     console.log(car3.model);
+//     console.log(car3.year);
+//     console.log(car3.color);
+//     console.log(car3.def);
+
+    class School{
+        constructor(Name,fathername,age, rate,city,country){
+            this.Name = Name;
+            this.fathername = fathername,
+            this.age = age,
+            this.rate = rate;
+            this.city= city;
+            this.country= country;
+        }
+
+    }
+    let Maktab1 = new School("MirAhmad","Noorahmad","32","3","jamsa","Finland");
+    let Maktab2 = new School("amir","Noorahmad","32","3","jamsa","sweedan");
+    let Maktab3 = new School("Rahila","Noorahmad","32","3","jamsa","afghanistan");
+    let Maktab4 = new School("Neamatulla","Noorahmad","32","3","crachi","Pakistan");
+    let Maktab5 = new School("Sayed ahmad","Noorahmad","32","3","khilar","afghanistan");
+
+    console.log(Maktab1.Name);
+    console.log(Maktab1.fathername);
+    console.log(Maktab1.age);
+    console.log(Maktab1.rate);
+    console.log(Maktab1.city);
+    console.log(Maktab1.country);
+
+    console.log(Maktab2.Name);
+    console.log(Maktab2.fathername);
+    console.log(Maktab2.age);
+    console.log(Maktab2.rate);
+    console.log(Maktab2.city);
+    console.log(Maktab2.country);
+
+    console.log(Maktab3.Name);
+    console.log(Maktab3.fathername);
+    console.log(Maktab3.age);
+    console.log(Maktab3.rate);
+    console.log(Maktab3.city);
+    console.log(Maktab3.country);
